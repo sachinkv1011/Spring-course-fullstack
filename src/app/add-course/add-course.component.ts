5e71dec88d5ff93fb1ce0f525a5b5dabbb9e9b37
SACHIN K V <sachin.kv.1011@gmail.com>
2022-12-06 22:12:12 +0530

    course added

---
+++ src/app/add-course/add-course.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-course',
+  templateUrl: './add-course.component.html',
+  styleUrls: ['./add-course.component.css']
+})
+export class AddCourseComponent {
+
+  title=""
+  description=""
+  venue=""
+  duration=""
+  date=""
+
+ 
+  readValue(){
+    let course:any={
+      "title":this.title,
+      "description":this.description,
+      "venue":this.venue,
+      "duration":this.duration,
+      "date":this.date
+    }
+
+    console.log(course)
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddCourseComponent } from './add-course/add-course.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddCourseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
